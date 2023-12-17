6141d332f7a5d06569ba6b229e5e92471cde9330
wefixte <cassy.pernelle@gmail.com>
2023-12-17 15:09:21 +0100

    Add main.h header file with librairies & env variable

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Librairies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/*Environment variable*/
+extern char **environ;
+
+/*Prototypes*/
+
+
+#endif
