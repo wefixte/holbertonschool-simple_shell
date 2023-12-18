2b3ff3ae6d09f52ae899aeb1f5af58909567f8dd
wefixte <cassy.pernelle@gmail.com>
2023-12-18 11:05:49 +0100

    Add shell.c file with main function

---
+++ shell.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * main - main function for shell
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+*/
+
+int main(int argc, char **argv)
+{
+	char *command = NULL;
+	size_t bufsize = MAX_COMMAND_LENGTH;
+
+	if (argc != 1)
+	{
+		perror("Number of arguments is incorrect");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		/*issaty: check if stdin come from an actif terminal*/
+		if (isatty(STDIN_FILENO) == 0)
+		{
+			perror("Not a terminal");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("#cisfun$ ");
+		fflush(stdout);
+
+		/*getline*/
+		if (getline(&command, &bufsize, stdin) == -1)
+			break;
+
+		/*remove newline*/
+		command[strcspn(command, "\n")] = '\0';
+
+		/*exit*/
+		if (strcmp(command, "exit\n") == 0)
+			break;
+
+		/*execute*/
+		if (execute(command) == -1)
+		{
+			perror("Command execution failed");
+		}
+	}
+	free(command);
+	return (0);
+}
