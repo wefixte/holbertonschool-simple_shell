3de1c13a46d0220315c5e9226bb807ade5605322
wefixte <cassy.pernelle@gmail.com>
2023-12-18 17:19:26 +0100

    Add execute_command function to execute a command by tokenizing it, determining the path, and using `execve` to execute the command.

---
+++ execute_command.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * execute_command - Function to execute the command
+ * @command: Pointer to array of strings representing the command to executed
+ * Return: 1 if successfull
+ */
+
+int execute_command(char *command)
+{
+	char **argument, *path;
+
+	argument = tokenize(command);
+	if (argument == NULL)
+	{
+		free(argument);
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+
+	if (command[0] == '/' || command[0] == '.')
+	{
+		path = strdup(command);
+	}
+	else
+	{
+		path = getpath(command);
+	}
+
+	if (path == NULL)
+	{
+		free(path);
+		free(argument);
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+
+	if (execve(command, argument, environ) == -1)
+	{
+		free(path);
+		free(argument);
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	return (1);
+}
