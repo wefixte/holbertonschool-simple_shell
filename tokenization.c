3802b16f1bacaf2f045b8ba4d6787c446e929de9
wefixte <cassy.pernelle@gmail.com>
2023-12-22 10:46:28 +0100

    Add documentation and commentaries

--- tokenization.c
+++ tokenization.c
@@ -10,23 +10,26 @@ char **tokenize(char *input)
 	char **args = NULL;
 	int argCount = 0;
 
+	/* Allocate memory for arguments */
 	args = malloc(sizeof(char *));
 	if (args == NULL)
 	{
 		perror("Memory allocation failed");
 		exit(EXIT_FAILURE);
 	}
-	token = strtok(input, " \t\n");
+	token = strtok(input, " \t\n"); /* Tokenize input using delimiters*/
+
 	while (token != NULL)
 	{
-		args[argCount] = strdup(token);
+		args[argCount] = strdup(token); /* Copy each token to args */
 		if (args[argCount] == NULL)
 		{
 			perror("Memory allocation failed");
 			exit(EXIT_FAILURE);
 		}
 		argCount++;
-		token = strtok(NULL, " \t\n");
+		token = strtok(NULL, " \t\n"); /* Get next token */
+		/*Reallocate memory for next element*/
 		args = realloc(args, (argCount + 1) * sizeof(char *));
 		if (args == NULL)
 		{
@@ -34,8 +37,9 @@ char **tokenize(char *input)
 			exit(EXIT_FAILURE);
 		}
 	}
+	/* Set last element of args to NULL */
 	args[argCount] = NULL;
-	if (argCount == 0)
+	if (argCount == 0) /*Free memory if no argument find*/
 	{
 		free(args);
 		return (NULL);
