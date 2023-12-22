4414fd133f0173f9c8c3bb0f31d6a1f04d51d04b
wefixte <cassy.pernelle@gmail.com>
2023-12-22 10:18:28 +0100

    Add commentary

--- getpath.c
+++ getpath.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * getpath - get the path of the command
+ * getpath - get the full path of a command
  * @input: command enter by the user
  * Return: returns the full path if found in the PATH
  * OR returns the command if not found
@@ -12,37 +12,41 @@ char *getpath(char *input)
 	char *path, *token, *copypath, *entirepath = NULL;
 	int length;
 
-	path = getenv("PATH");
+	path = getenv("PATH"); /*search for environment strings*/
 	if (path == NULL || strlen(path) == 0)
 		return (strdup(input));
 
-	copypath = strdup(path);
-	token = strtok(path, ":");
+	copypath = strdup(path); /*Return a pointer to a new string duplicate*/
+	token = strtok(path, ":"); /*breaks string into a series of tokens*/
 
+	/*loop through the string to get the full path*/
 	while (token != NULL)
 	{
-		length = strlen(token) + strlen(input) + 2;
-		entirepath = malloc(sizeof(char) * (length + 1));
+		/*Allocate memory for the entire path*/
+		length = strlen(token) + strlen(input) + 2; /*+2 for the / & the null*/
+		entirepath = malloc(sizeof(char) * (length + 1)); /*+1 for the null byte*/
 
-		if (entirepath == NULL)
+		if (entirepath == NULL) /*if malloc fails*/
 		{
 			free(copypath);
-			perror("Error");
+			perror("Error in memory allocation");
 			exit(EXIT_FAILURE);
 		}
 
-		entirepath = strcpy(entirepath, token);
-		entirepath = strcat(entirepath, "/");
-		entirepath = strcat(entirepath, input);
+		/*copy the path and the command into the entire path*/
+		strcpy(entirepath, token);
+		strcat(entirepath, "/");
+		strcat(entirepath, input);
 
+		/*Checks whether the calling process can access the file pathname*/
 		if (access(entirepath, F_OK | X_OK) == 0)
 		{
 			free(copypath);
 			return (entirepath);
 		}
 		free(entirepath);
-		token = strtok(NULL, ":");
+		token = strtok(NULL, ":"); /*returns a pointer to the next token*/
 	}
 	free(copypath);
-	return (strdup(input));
+	return (strdup(input)); /*Return a pointer to a new string duplicate*/
 }
